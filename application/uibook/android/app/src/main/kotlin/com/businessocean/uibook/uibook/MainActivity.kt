b1e469c1fe3b12d1dfb8e8869baf4a50a6f38560
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2023-06-07 00:58:24 +0530

    init setup

---
+++ application/uibook/android/app/src/main/kotlin/com/businessocean/uibook/uibook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.businessocean.uibook.uibook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/bego_component/lib/bego_component.dart
@@ -0,0 +1,7 @@
+library bego_component;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
