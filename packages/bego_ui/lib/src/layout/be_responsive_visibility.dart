a9cdb09b14342f5358e42bd473e65a52082ac26f
Sourav pandit <77164607+BusinessOcean@users.noreply.github.com>
2023-06-10 00:58:04 +0530

    added responsive visibility and value (#11)

---
+++ packages/bego_ui/lib/src/layout/be_responsive_visibility.dart
@@ -0,0 +1,23 @@
+import 'package:bego_core/bego_get.dart';
+import 'package:bego_ui/src/common_widget.dart';
+import 'package:bego_ui/src/layout/breakpoint.dart';
+import 'package:flutter/widgets.dart';
+
+/// First value will be the default value
+final class BeResponsiveVisibility {
+  BeResponsiveVisibility({required this.child, this.onVisible});
+  final Map<Breakpoint, bool>? onVisible;
+  final Widget child;
+  Widget resolve(BuildContext context) {
+    if (onVisible == null) return child;
+    final isVisible = switch (context.mediaQuery.screenBreakPoint) {
+          Breakpoint.extraSmall => onVisible![Breakpoint.extraSmall],
+          Breakpoint.small => onVisible![Breakpoint.small],
+          Breakpoint.medium => onVisible![Breakpoint.medium],
+          Breakpoint.large => onVisible![Breakpoint.large],
+          Breakpoint.extraLarge => onVisible![Breakpoint.extraLarge],
+        } ??
+        true;
+    return isVisible ? child : zeroWidget;
+  }
+}
