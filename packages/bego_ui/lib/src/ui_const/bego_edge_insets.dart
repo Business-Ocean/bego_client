325aa8f0910968d6add11adf1cd98a27e560678b
Sourav pandit <77164607+BusinessOcean@users.noreply.github.com>
2023-06-13 01:51:25 +0530

    Bego 11 add tailwind based padding and margin (#20)
    * added  padding and spacing const
    
    * analysis fix

---
+++ packages/bego_ui/lib/src/ui_const/bego_edge_insets.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+const p1 = EdgeInsets.all(1);
+const p2 = EdgeInsets.all(2);
+const p4 = EdgeInsets.all(4);
+const p8 = EdgeInsets.all(8);
+const p12 = EdgeInsets.all(12);
+const p16 = EdgeInsets.all(16);
+const p18 = EdgeInsets.all(18);
+const p24 = EdgeInsets.all(24);
+const p32 = EdgeInsets.all(32);
+
+const px2 = EdgeInsets.symmetric(horizontal: 2);
+const px4 = EdgeInsets.symmetric(horizontal: 4);
+const px8 = EdgeInsets.symmetric(horizontal: 8);
+const px12 = EdgeInsets.symmetric(horizontal: 12);
+const px16 = EdgeInsets.symmetric(horizontal: 16);
+const px18 = EdgeInsets.symmetric(horizontal: 18);
+const px24 = EdgeInsets.symmetric(horizontal: 24);
+const px32 = EdgeInsets.symmetric(horizontal: 32);
+
+const py2 = EdgeInsets.symmetric(vertical: 2);
+const py4 = EdgeInsets.symmetric(vertical: 4);
+const py8 = EdgeInsets.symmetric(vertical: 8);
+const py12 = EdgeInsets.symmetric(vertical: 12);
+const py16 = EdgeInsets.symmetric(vertical: 16);
+const py18 = EdgeInsets.symmetric(vertical: 18);
+const py24 = EdgeInsets.symmetric(vertical: 24);
+const py32 = EdgeInsets.symmetric(vertical: 32);
+
+const pt2 = EdgeInsets.only(top: 2);
+const pt4 = EdgeInsets.only(top: 4);
+const pt8 = EdgeInsets.only(top: 8);
+const pt12 = EdgeInsets.only(top: 12);
+const pt16 = EdgeInsets.only(top: 16);
+const pt18 = EdgeInsets.only(top: 18);
+const pt24 = EdgeInsets.only(top: 24);
+const pt32 = EdgeInsets.only(top: 32);
+
+const pb2 = EdgeInsets.only(bottom: 2);
+const pb4 = EdgeInsets.only(bottom: 4);
+const pb8 = EdgeInsets.only(bottom: 8);
+const pb12 = EdgeInsets.only(bottom: 12);
+const pb16 = EdgeInsets.only(bottom: 16);
+const pb18 = EdgeInsets.only(bottom: 18);
+const pb24 = EdgeInsets.only(bottom: 24);
+const pb32 = EdgeInsets.only(bottom: 32);
+
+const pl2 = EdgeInsets.only(left: 2);
+const pl4 = EdgeInsets.only(left: 4);
+const pl8 = EdgeInsets.only(left: 8);
+const pl12 = EdgeInsets.only(left: 12);
+const pl16 = EdgeInsets.only(left: 16);
+const pl18 = EdgeInsets.only(left: 18);
+const pl24 = EdgeInsets.only(left: 24);
+const pl32 = EdgeInsets.only(left: 32);
+
+const pr2 = EdgeInsets.only(right: 2);
+const pr4 = EdgeInsets.only(right: 4);
+const pr8 = EdgeInsets.only(right: 8);
+const pr12 = EdgeInsets.only(right: 12);
+const pr16 = EdgeInsets.only(right: 16);
+const pr18 = EdgeInsets.only(right: 18);
+const pr24 = EdgeInsets.only(right: 24);
+const pr32 = EdgeInsets.only(right: 32);
---
+++ packages/bego_ui/lib/src/ui_const/bego_gap.dart
@@ -0,0 +1,11 @@
+import 'package:bego_ui/src/ui_const/bego_edge_insets.dart';
+import 'package:flutter/material.dart';
+
+Padding get space2 => const Padding(padding: pb2);
+Padding get space4 => const Padding(padding: pb4);
+Padding get space8 => const Padding(padding: pb8);
+Padding get space12 => const Padding(padding: pb12);
+Padding get space16 => const Padding(padding: pb16);
+Padding get space18 => const Padding(padding: pb18);
+Padding get space24 => const Padding(padding: pb24);
+Padding get space32 => const Padding(padding: pb32);
