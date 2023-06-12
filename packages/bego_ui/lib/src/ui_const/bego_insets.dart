882f0af8ce7e7f104b9b526835e4b46d1cedc0d6
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2023-06-13 01:37:15 +0530

    added  padding and spacing const

--- packages/bego_ui/lib/src/ui_const/bego_insets.dart
+++ packages/bego_ui/lib/src/ui_const/bego_insets.dart
@@ -1,9 +1,3 @@
-import 'package:flutter/material.dart';
-
 final class BegoInsets {
   const BegoInsets._();
-
-  static const textSpaceing = 4.0;
-  static const inset2Vertical = EdgeInsets.symmetric(vertical: 2);
-  static const inset2 = EdgeInsets.all(2);
 }
