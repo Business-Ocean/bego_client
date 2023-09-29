bbb9e426df4fdf4669ff3d824188f2f7c0360ef3
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2023-09-29 13:14:34 +0530

    removed hive for using isar db

--- application/uibook/lib/widgets/notification/badge.dart
+++ application/uibook/lib/widgets/notification/badge.dart
@@ -1,4 +1,4 @@
-import 'package:bego_ui/bego_ui.dart';
+import 'package:bego_ui/bego_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
