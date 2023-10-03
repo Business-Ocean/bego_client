3af666730d985fe1a95de041b8ccd81e9b3851e3
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2023-10-03 15:47:34 +0530

    added label_icon.dar

--- packages/bego_ui/lib/bego_widgets.dart
+++ packages/bego_ui/lib/bego_widgets.dart
@@ -7,6 +7,7 @@ export 'src/widgets/container/be_invert_color.dart';
 export 'src/widgets/form/be_text_field.dart';
 export 'src/widgets/notice/be_badge.dart';
 export 'src/widgets/notice/be_input_label.dart';
+export 'src/widgets/text/be_label_icon.dart';
 export 'src/widgets/text/be_text.dart';
 export 'src/widgets/text/be_text_tagged.dart';
 export 'src/widgets/text/be_text_type.dart';
---
+++ packages/bego_ui/lib/src/widgets/text/be_label_icon.dart
@@ -0,0 +1,82 @@
+import 'package:bego_ui/bego_ui.dart';
+import 'package:bego_ui/bego_widgets.dart';
+import 'package:flutter/material.dart';
+
+class BeLabelIcon extends StatelessWidget {
+  const BeLabelIcon({
+    super.key,
+    required this.text,
+    this.color,
+    this.leading,
+    this.label,
+    this.trailing,
+    this.isMandatory = false,
+    this.onTrailingTap,
+    this.onleadingTap,
+    this.textStyle,
+    this.hintStyle,
+    this.hintPosition = BeBadgePosition.centerRight,
+  });
+  final Color? color;
+  final String text;
+  final String? label;
+  final Widget? leading;
+  final Widget? trailing;
+  final bool isMandatory;
+  final VoidCallback? onleadingTap;
+  final VoidCallback? onTrailingTap;
+
+  final TextStyle? textStyle;
+  final TextStyle? hintStyle;
+  final BeBadgePosition hintPosition;
+
+  @override
+  Widget build(BuildContext context) => BeTextTagged(
+        label: label,
+        position: hintPosition,
+        tagColor: BegoColors.gray300,
+        tagBackground: BegoColors.transparent,
+        tagStyle: hintStyle ?? const TextStyle(fontSize: 14),
+        child: RichText(
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          // textAlign: TextAlign.center,
+          text: TextSpan(
+            style: const TextStyle(),
+            children: [
+              if (leading != null) ...[
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: InkWell(
+                    onTap: onleadingTap,
+                    splashFactory: const NoSplashFactory(),
+                    child: leading!,
+                  ),
+                ),
+                WidgetSpan(child: space2),
+              ],
+              TextSpan(
+                text: text,
+                style: TextStyle(color: becolors(context).text),
+              ),
+              if (isMandatory)
+                TextSpan(
+                  text: '*',
+                  style: TextStyle(color: becolors(context).primary),
+                ),
+              if (trailing != null) ...[
+                WidgetSpan(child: space2),
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: InkWell(
+                    onTap: onTrailingTap,
+                    splashFactory: const NoSplashFactory(),
+                    child: trailing!,
+                  ),
+                ),
+              ],
+            ],
+          ),
+        ),
+      );
+}
