c67c244c26b3aa4c849356034052f63491f0ebe9
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2023-11-12 00:28:11 +0530

    Update badge.dart

--- application/uibook/lib/widgets/notification/badge.dart
+++ application/uibook/lib/widgets/notification/badge.dart
@@ -1,6 +1,7 @@
 import 'package:bego_ui/bego_ui.dart';
 import 'package:bego_ui/bego_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(name: 'BeBadge', type: BeBadge)
@@ -21,14 +22,14 @@ Widget displayBeBadgeUseCase(BuildContext context) {
                         position: e,
                         rounded: true,
                         badge: Container(
-                          decoration: const BoxDecoration(
+                          decoration: BoxDecoration(
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(16)),
-                              color: Colors.orange),
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(
+                                  const BorderRadius.all(Radius.circular(4)),
+                              color: ColorUtils.getRandomColor()),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
                                 horizontal: 8, vertical: 2),
-                            child: Text("0"),
+                            child: Text(e.name),
                           ),
                         ),
                         child: Container(
@@ -53,6 +54,11 @@ Widget displayBeBadgeUseCase(BuildContext context) {
 
 @widgetbook.UseCase(name: 'BeLabel', type: BeLabel)
 Widget displayBeLabelUseCase(BuildContext context) {
+  final inner =
+      context.knobs.boolean(label: "Inner Label", initialValue: false);
+  final fullWidth =
+      context.knobs.boolean(label: "Label Full Width", initialValue: false);
+
   return Scaffold(
     body: Container(
       alignment: Alignment.center,
@@ -66,15 +72,20 @@ Widget displayBeLabelUseCase(BuildContext context) {
                       margin: const EdgeInsets.symmetric(
                           vertical: 16, horizontal: 48),
                       child: BeLabel(
+                        childSized: fullWidth,
+
+                        innerLabel: inner,
                         position: e,
                         // rounded: true,
                         label: Container(
-                          width: 20,
-                          height: 20,
                           decoration: const BoxDecoration(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(4)),
                               color: Colors.orange),
+                          child: const SizedBox(
+                            width: 20,
+                            height: 20,
+                          ),
                         ),
                         child: Container(
                           height: 50,
@@ -97,24 +108,33 @@ Widget displayBeLabelUseCase(BuildContext context) {
 
 @widgetbook.UseCase(name: 'BeMultiBadge', type: BeMultiBadge)
 Widget displayBeMultiBadgeUseCase(BuildContext context) {
+  final rounded = context.knobs.boolean(label: "Rounded", initialValue: false);
+
   return Scaffold(
     body: Container(
       alignment: Alignment.center,
       child: BeMultiBadge(
+          rounded: rounded,
           labels: BeMultiBadgePosition.values
-              .map((e) => BeBadgeChild(
+              .map(
+                (e) => BeBadgeChild(
                   position: e,
                   child: Container(
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(16)),
-                        color: Colors.orange),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                      child: Text("0"),
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(4)),
+                        color: ColorUtils.getRandomColor()),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 2),
+                      child: Text(e.name),
                     ),
-                  )))
+                  ),
+                ),
+              )
               .toList(),
-          child: Container(color: Colors.pink, width: 400, height: 300)),
+          child: Container(
+              color: Colors.black.withAlpha(50), width: 400, height: 300)),
     ),
   );
 }
@@ -126,17 +146,22 @@ Widget displayBeMultiLabelUseCase(BuildContext context) {
       alignment: Alignment.center,
       child: BeMultiLabel(
           labels: BeMultiLabelPosition.values
-              .map((e) => BeLabelChild(
+              .map(
+                (e) => BeLabelChild(
                   position: e,
                   child: Container(
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(16)),
-                        color: Colors.orange),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                      child: Text("0"),
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(4)),
+                        color: ColorUtils.getRandomColor()),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 2),
+                      child: Text(e.name),
                     ),
-                  )))
+                  ),
+                ),
+              )
               .toList(),
           child: Container(color: Colors.pink, width: 400, height: 300)),
     ),
