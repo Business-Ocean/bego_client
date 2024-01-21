78188e3179616ae85ce76ac65cb2837232743bff
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2024-01-21 21:18:43 +0530

    pub upgrade

--- packages/bego_ui/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ packages/bego_ui/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/businessocean/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/businessocean/development/businessocean/bego-client/packages/bego_ui"
+export "FLUTTER_APPLICATION_PATH=/home/businessocean/development/businessocean/bego_client/packages/bego_ui"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
